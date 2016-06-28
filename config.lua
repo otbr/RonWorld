e0e375cd02bdc0fb5ce1f66d791ddff56cc29e06
ronpetit <elronaldpetit@gmail.com>
2016-06-28 14:18:50 +0000

    Added RonWorld Map, added definitions and change items setup

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 -- Combat settings
 -- NOTE: valid values for worldType are: "pvp", "no-pvp" and "pvp-enforced"
 worldType = "pvp"
-hotkeyAimbotEnabled = "yes"
+hotkeyAimbotEnabled = "no"
 protectionLevel = 1
 killsToRedSkull = 3
 killsToBlackSkull = 6
@@ -9,22 +9,22 @@ pzLocked = 60000
 removeChargesFromRunes = "yes"
 timeToDecreaseFrags = 24 * 60 * 60 * 1000
 whiteSkullTime = 15 * 60 * 1000
-stairJumpExhaustion = 2000
+stairJumpExhaustion = 100
 experienceByKillingPlayers = "no"
 expFromPlayersLevelRange = 75
 
 -- Connection Config
 -- NOTE: maxPlayers set to 0 means no limit
-ip = "127.0.0.1"
+ip = "192.168.2.129"
 bindOnlyGlobalAddress = "no"
 loginProtocolPort = 7171
 gameProtocolPort = 7172
 statusProtocolPort = 7171
-maxPlayers = 0
-motd = "Welcome to The Forgotten Server!"
+maxPlayers = 50
+motd = "Welcome to RonWorld!"
 onePlayerOnlinePerAccount = "yes"
 allowClones = "no"
-serverName = "Forgotten"
+serverName = "RonWorld"
 statusTimeout = 5000
 replaceKickOnLogin = "yes"
 maxPacketsPerSecond = 25
@@ -37,7 +37,7 @@ deathLosePercent = -1
 
 -- Houses
 -- NOTE: set housePriceEachSQM to -1 to disable the ingame buy house functionality
-housePriceEachSQM = 1000
+housePriceEachSQM = 2500
 houseRentPeriod = "never"
 
 -- Item Usage
@@ -46,8 +46,8 @@ timeBetweenExActions = 1000
 
 -- Map
 -- NOTE: set mapName WITHOUT .otbm at the end
-mapName = "forgotten"
-mapAuthor = "Komic"
+mapName = "RonWorld"
+mapAuthor = "Ron"
 
 -- Market
 marketOfferDuration = 30 * 24 * 60 * 60
@@ -57,9 +57,9 @@ maxMarketOffersAtATimePerPlayer = 100
 
 -- MySQL
 mysqlHost = "127.0.0.1"
-mysqlUser = "forgottenserver"
-mysqlPass = ""
-mysqlDatabase = "forgottenserver"
+mysqlUser = "root"
+mysqlPass = "RONron56"
+mysqlDatabase = "RonWorld"
 mysqlPort = 3306
 mysqlSock = ""
 
@@ -69,14 +69,14 @@ freePremium = "no"
 kickIdlePlayerAfterMinutes = 15
 maxMessageBuffer = 4
 emoteSpells = "no"
-classicEquipmentSlots = "no"
+classicEquipmentSlots = "yes"
 
 -- Rates
 -- NOTE: rateExp is not used if you have enabled stages in data/XML/stages.xml
-rateExp = 5
-rateSkill = 3
-rateLoot = 2
-rateMagic = 3
+rateExp = 30
+rateSkill = 10
+rateLoot = 3
+rateMagic = 7
 rateSpawn = 1
 
 -- Monsters
@@ -97,7 +97,7 @@ defaultPriority = "high"
 startupDatabaseOptimization = "no"
 
 -- Status server information
-ownerName = ""
-ownerEmail = ""
-url = "http://otland.net/"
-location = "Sweden"
+ownerName = "Ron"
+ownerEmail = "elronaldpetit@gmail.com"
+url = "http://otland.net/raiden"
+location = "USA"
